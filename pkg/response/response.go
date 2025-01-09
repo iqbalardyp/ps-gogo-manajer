86f0dbeae3538d286496095f1a0016c9136d7021
dohardy <ridhoadha117@bebasaja.com>
2025-01-09 23:13:00 +0700

    add: add custom error dari repo fadil

--- pkg/response/response.go
+++ pkg/response/response.go
@@ -1,5 +1,7 @@
 package response
 
+import "net/http"
+
 type HealthCheck struct {
 	AppName string `json:"app_name"`
 	Status  string `json:"status"`
@@ -7,4 +9,51 @@ type HealthCheck struct {
 
 type ErrorResponse struct {
 	Message string `json:"message"`
+}
+
+
+
+type CustomError struct {
+	Message    string `json:"message"`
+	StatusCode int    `json:"statusCode"`
+}
+
+// implement error interface
+func (c *CustomError) Error() string {
+	return c.Message
+}
+
+func Conflict(msg string) *CustomError {
+	return &CustomError{
+		Message:    msg,
+		StatusCode: http.StatusConflict,
+	}
+}
+
+func NotFound(msg string) *CustomError {
+	return &CustomError{
+		Message:    msg,
+		StatusCode: http.StatusNotFound,
+	}
+}
+
+func BadRequest(msg string) *CustomError {
+	return &CustomError{
+		Message:    msg,
+		StatusCode: http.StatusBadRequest,
+	}
+}
+
+func Unauthorized(msg string) *CustomError {
+	return &CustomError{
+		Message:    msg,
+		StatusCode: http.StatusUnauthorized,
+	}
+}
+
+func ServerError(msg string) *CustomError {
+	return &CustomError{
+		Message:    msg,
+		StatusCode: http.StatusInternalServerError,
+	}
 }
