275ad8f3371815be9bddae720c41c1932bb690e5
iqbal-ap <iqbal.putra@bithealth.co.id>
2025-01-10 09:12:47 +0700

    feat: change to hard-delete schema & add password column in table users

--- db/migrations/000001_init_schema.up.sql
+++ db/migrations/000001_init_schema.up.sql
@@ -2,13 +2,11 @@
 CREATE TABLE users (
     id BIGSERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
+    password VARCHAR(255),
     email VARCHAR(255) UNIQUE NOT NULL,
     company_name VARCHAR(255),
     company_name_uri VARCHAR(255),
-    user_image_uri VARCHAR(255),
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    deleted_at TIMESTAMP WITH TIME ZONE
+    user_image_uri VARCHAR(255)
 );
 
 -- Create table departments
@@ -16,9 +14,6 @@ CREATE TABLE departments (
     id BIGSERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     user_id BIGINT NOT NULL,
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    deleted_at TIMESTAMP WITH TIME ZONE,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
@@ -34,35 +29,7 @@ CREATE TABLE employees (
     employee_image_uri VARCHAR(255),
     department_id BIGINT NOT NULL,
     user_id BIGINT NOT NULL,
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    deleted_at TIMESTAMP WITH TIME ZONE,
     FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE CASCADE,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
     CONSTRAINT employees_identity_number_per_user UNIQUE (user_id, identity_number)
 );
-
--- Create updated_at trigger function
-CREATE OR REPLACE FUNCTION trigger_set_timestamp()
-RETURNS TRIGGER AS $$
-BEGIN
-  NEW.updated_at = NOW();
-  RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
--- Create triggers
-CREATE TRIGGER set_timestamp_users
-    BEFORE UPDATE ON users
-    FOR EACH ROW
-    EXECUTE FUNCTION trigger_set_timestamp();
-
-CREATE TRIGGER set_timestamp_departments
-    BEFORE UPDATE ON departments
-    FOR EACH ROW
-    EXECUTE FUNCTION trigger_set_timestamp();
-
-CREATE TRIGGER set_timestamp_employees
-    BEFORE UPDATE ON employees
-    FOR EACH ROW
-    EXECUTE FUNCTION trigger_set_timestamp();
